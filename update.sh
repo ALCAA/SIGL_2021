6c4ae90c62d786e857757074602271316202f5b2
Antoine TROLLET <antoine.trollet@epita.fr>
2020-02-27 10:35:57 +0100

    [MISC] mise à jour du script pour les index.md

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
+#!/bin/sh
+
 printf "# SIGL 2021 Index page # \n \n" > index.md
 for f in *; do
     if [ -d "$f" ]; then
-	echo -e "- [$f]($f)" >> index.md
+	echo "- [$f]($f)" >> index.md
 	cd "$f"
 	printf "# $f Index page # \n \n" > index.md
 	for file in *; do
 		if ! cmp -s "$file" "index.md" ; then
-			echo -e "- [${file%.*}]($file)" >> index.md
+			echo "- [${file%.*}]($file)" >> index.md
 		fi
 	done
 	cd ..
